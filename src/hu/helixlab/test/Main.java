bce6aebf004e5ab0f8629dcd2158728a319ca663
Farkas Balazs <farkas0528@gmail.com>
2017-03-12 16:41:45 +0100

    Téglalap terület #1

--- src/hu/helixlab/test/Main.java
+++ src/hu/helixlab/test/Main.java
@@ -3,6 +3,12 @@ package hu.helixlab.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // felveszek 2 short változót a téglalap két oldalának tárolására
+
+        short a = 5;
+        short b = 4;
+
+        System.out.println("A téglalap területe: " + a * b + " cm^2");
+        System.out.println("A téglalap kerülete: " + (a + b) * 2 + " cm\n");
     }
 }
