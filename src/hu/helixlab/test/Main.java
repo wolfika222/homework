6831c9bde52a7e1a92727aad5340815115ce5560
Farkas Balazs <farkas0528@gmail.com>
2017-03-12 16:43:16 +0100

    Deciliter #2

--- src/hu/helixlab/test/Main.java
+++ src/hu/helixlab/test/Main.java
@@ -10,5 +10,16 @@ public class Main {
 
         System.out.println("A téglalap területe: " + a * b + " cm^2");
         System.out.println("A téglalap kerülete: " + (a + b) * 2 + " cm\n");
+
+         /* deciliteres feladat
+        felveszek egy short típusú változót a deciliternek
+         */
+
+        short dl = 20;
+
+        System.out.println(dl + " dl = " + dl * 100 + " ml");
+        System.out.println(dl + " dl = " + dl * 10 + " cl");
+        System.out.println(dl + " dl = " + dl / 10 + " l");
+        System.out.println(dl + " dl = " + dl / 1000 + " hl\n");
     }
 }
